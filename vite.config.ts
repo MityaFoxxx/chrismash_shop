61fde04ad5ea24348daba1e10016de8a65421c8d
MityaFoxxx <89162246661fox@gmail.com>
2026-04-05 16:03:01 +0300

    chore: Добавил сжатие картинок на production

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,59 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
+import tailwindcss from '@tailwindcss/vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    tailwindcss(),
+    ViteImageOptimizer({
+      // Настройки для разных форматов
+      png: {
+        quality: 80,
+        compressionLevel: 9,
+      },
+      jpg: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      webp: {
+        lossless: false,
+        quality: 80,
+      },
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+                removeViewBox: false,
+              },
+            },
+          },
+          'removeDimensions',
+          'sortAttrs',
+        ],
+      },
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name?.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType || '')) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+      },
+    },
+  },
 });
