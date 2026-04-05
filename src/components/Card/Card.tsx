5269a8e193b8fbde98b58cb0440a370dd37752e1
MityaFoxxx <89162246661fox@gmail.com>
2026-04-05 14:18:32 +0300

    fix: одинаковая высота карточек

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,18 +10,18 @@ export const Card = ({ item, handleClickCard }: CardProps) => {
   return (
     <article
       key={item.id}
-      className="flex flex-col bg-gray-200 w-77.5 rounded-2xl cursor-pointer overflow-hidden hover:scale-102 transition-transform"
+      className="flex flex-col bg-gray-200 w-77.5 rounded-2xl cursor-pointer overflow-hidden hover:scale-102 transition-transform h-full"
       onClick={() => handleClickCard && handleClickCard(item)}
     >
       <div className="shrink-0">
         <img
           src={item.imgUrl}
           alt={item.title}
-          className="bg-gray-200 w-full rounded-tl-2xl rounded-tr-2xl"
+          className="bg-gray-200 w-full aspect-[4/3] object-cover rounded-tl-2xl rounded-tr-2xl"
         />
       </div>
 
-      <div className="px-5 pt-4.5 pb-10 flex flex-col gap-2.5 bg-white grow">
+      <div className="px-5 pt-4.5 pb-10 flex flex-col gap-2.5 bg-white flex-1">
         <p
           className="text-[12px] font-semibold tracking-[5%] uppercase m-0"
           style={{
